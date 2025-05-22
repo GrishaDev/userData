daae311712a91b4986b043e5f7b9b78483222525
grisha <codpure@gmail.com>
2025-05-22 17:13:44 +0300

    docker stuff

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-// const corsOptions = {
-//   origin: ['http://localhost:80', process.env.FE_URL],
-//   credentials: true,
-// };
+// Resolves CORS issues during development, can be removed later.
+const corsOptions = {
+  origin: [`http://localhost:${process.env.PORT}`, process.env.FE_URL],
+  credentials: true,
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors(corsOptions);
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
